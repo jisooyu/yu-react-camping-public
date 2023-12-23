c7c1c61d4983ae3b66652dcd0891f6059d486d87
jisooyu <jisooyu@hotmail.com>
2023-12-23 17:09:56 +0900

    material-tailwind/react

--- client/src/components/CampingDetail.js
+++ client/src/components/CampingDetail.js
@@ -1,4 +1,4 @@
-import { Carousel } from '@material-tailwind/react';
+import { Carousel, Typography } from '@material-tailwind/react';
 
 function CampingDetail({ campingData }) {
 	let content;
@@ -7,33 +7,59 @@ function CampingDetail({ campingData }) {
 			<Carousel className='rounded-xl'>
 				{campingData.map((campingItem) => (
 					<div key={campingItem.contentId}>
-						<div class='relative flex flex-col mt-6 text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96'>
-							<div class='relative h-56 mx-4 -mt-6 overflow-hidden text-white shadow-lg bg-clip-border rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40'>
-								<img
-									src={campingItem.firstImageUrl || 'no-image-available.jpeg'}
-									alt='campingPicture'
-									className='h-4/5 w-full object-cover'
-								/>
+						<div className='relative h-full w-full flex justify-center'>
+							<img
+								src={campingItem.firstImageUrl || 'no-image-available.jpeg'}
+								alt='campingPicture'
+								className='h-full w-full object-cover'
+							/>
+						</div>
+						<div className='absolute inset-0 grid h-full w-full place-items-center bg-black/75'>
+							<div className='w-3/4 text-center md:w-2/4'>
+								<div className='p-6 pt-0'>
+									<button
+										className='align-middle font-sans font-bold text-center uppercase py-3 px-6 rounded-lg bg-gray-900'
+										type='button'
+									>
+										<a
+											className='text-blue-500 hover:text-red-500 underline !important'
+											href={campingItem.homepage}
+										>
+											Home Page Link
+										</a>
+									</button>
+								</div>
 							</div>
 						</div>
-						<div class='p-6'>
-							<p>캠핑장이름:{campingItem.facltNm}</p>
-							<p>캠핑장 주소:{campingItem.addr1}</p>
-							<p>펫입장여부:{campingItem.animalCmgCl}</p>
-							<p>부대시설:{campingItem.caravInnerFclty || 'N.A.'}</p>
-						</div>
-						<div class='p-6 pt-0'>
-							<button
-								class='align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-lg bg-gray-900 text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none'
-								type='button'
+						<div className='w-full text-center md:w-2/4'>
+							<Typography
+								variant='h3'
+								color='blue'
+								className='mb-4 text-2xl md:text-4xl lg:text-5xl'
 							>
-								<a
-									className='text-blue-500 hover:text-red-500 underline !important'
-									href={campingItem.homepage}
-								>
-									Home Page Link
-								</a>
-							</button>
+								캠핑장:{campingItem.facltNm}
+							</Typography>
+							<Typography
+								variant='lead'
+								color='blue'
+								className='mb-12 opacity-80'
+							>
+								캠핑장 주소:{campingItem.addr1}
+							</Typography>
+							<Typography
+								variant='lead'
+								color='blue'
+								className='mb-12 opacity-80'
+							>
+								펫입장여부:{campingItem.animalCmgCl}
+							</Typography>
+							<Typography
+								variant='lead'
+								color='blue'
+								className='mb-12 opacity-80'
+							>
+								부대시설:{campingItem.caravInnerFclty || 'N.A.'}
+							</Typography>
 						</div>
 					</div>
 				))}
